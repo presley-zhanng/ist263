dce7b6134e103f68d38fe8fd8c076872e49dc8c8
presleyZhang <presley.zhang@gmail.com>
2024-05-05 17:06:18 -0700

    project update

--- project_final/script.js
+++ project_final/script.js
@@ -1,8 +1,15 @@
 function myFunction() {
-    var x = document.getElementById("main_nav");
-    if (x.className === "nav_bar") {
-      x.className += " responsive";
-    } else {
-      x.className = "nav_bar";
-    }
+  var x = document.getElementById("main_nav");
+  if (x.className === "nav_bar") {
+    x.className += " responsive";
+  } else {
+    x.className = "nav_bar";
   }
+}
+
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
